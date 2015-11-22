a1912d03df3c9938b7f52edf4112cf776e4305c3
Jamen Marz <jamenmarz@gmail.com>
2015-11-21 20:31:42 -0700

    Add value rendering tests

--- tests/render.js
+++ tests/render.js
@@ -1,8 +1,31 @@
 // jshint ignore: start
 jest.autoMockOff();
 
-const build = require('../lib/build');
+const lib = require('../lib'),
+      Value = lib.Value,
+      Property = lib.Property,
+      Rule = lib.Rule,
+      render = lib.render;
 
 describe('build function', () => {
+
+  it('renders Values', () => {
+    expect(
+      render.value(Value(2, 'px'))
+    ).toBe('2px');
+
+    expect(
+      render.value(Value('none'))
+    ).toBe('none');
+
+    expect(
+      render.value(Value('rgb', [
+        Value(0),
+        Value(1),
+        Value(2)
+      ]))
+    ).toBe('rgb(0, 1, 2)')
+
+  });
 
 });
