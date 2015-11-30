3ff70d83c71675e78f6a131175f369667d140839
Jamen Marz <jamenmarz@gmail.com>
2015-11-30 15:56:16 -0700

    Add lib/value.js test.

--- test/value.js
+++ test/value.js
@@ -1,2 +1,17 @@
 import test from 'ava';
 import { Value } from '../lib';
+
+test('"Value" object', ({same, true: isTrue}) => {
+  let numeric = new Value(0, 'px');
+  let keyword = new Value('none');
+
+  // Test numeric operation
+  same(numeric.plus(5, 'px'), new Value(5, 'px'));
+
+  // Test numeric operation on falsey keyword
+  isTrue(numeric.equals(keyword));
+
+  // Test tagging
+  numeric.set('foo-bar');
+  isTrue(numeric.has('foo-bar'));
+});
