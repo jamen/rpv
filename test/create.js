6f47c1df1e980ac680007a4af58f2f54c6829f6d
Jamen Marz <jamenmarz@gmail.com>
2015-11-30 15:08:32 -0700

    Add lib/create.js test

--- test/create.js
+++ test/create.js
@@ -1,2 +1,28 @@
 import test from 'ava';
 import { create } from '../lib';
+
+test('"create" function.', ({is, true: isTrue}) => {
+  // Create object.
+  let Test = create(function(a){
+    this.a = a;
+  }, {
+    foo: function(a){ this.a = a; }
+  });
+
+  // Initialize object.
+  let foo = new Test('baz');
+
+  // Check construction value.
+  is(foo.a, 'baz');
+
+  // Change value
+  foo.foo('qux'); // Haha, foofoo
+
+  // Test new value
+  is(foo.a, 'qux');
+
+  // Construction without "new"
+  // jshint newcap: false
+  let bar = Test('qux');
+  isTrue(bar instanceof Test);
+});
