14d755982afb6331c69764cc21de15d573fc44e6
Jamen Marz <jamenmarz@gmail.com>
2015-11-30 15:29:32 -0700

    Add rule test

--- test/rule.js
+++ test/rule.js
@@ -1,2 +1,38 @@
 import test from 'ava';
-import { Rule, Property, Value } from '../lib';
+import { render, Rule, Property, Value } from '../lib';
+
+test('"Rule" object', ({is, true: isTrue}) => {
+  let
+  selrule = new Rule('.foo', [
+    new Property('margin', [
+      new Value(10, 'px'),
+      new Value(2, 'em')
+    ])
+  ]),
+
+  corule = new Rule('@media', [
+    new Property('max-width', [ new Value(600, 'px') ]),
+    'and',
+    new Property('min-width', [ new Value(200, 'px') ])
+  ], [
+    selrule
+  ]),
+
+  nocorule = new Rule('@import', [ new Value('url', [ 'http://example.com/foo.css' ]) ]);
+
+  // Test tags
+  selrule.set('foo-bar');
+  isTrue(selrule.has('foo-bar'));
+
+
+  // Render of selrule
+  is(render(selrule), '.foo {margin:10px 2em}');
+
+  // Render of corule
+  is(render(corule), '@media (max-width:600px) and (min-width:200px) {.foo {margin:10px 2em}}');
+
+  // Render of nocorule
+  is(render(nocorule), '@import url(http://example.com/foo.css)');
+
+
+});
