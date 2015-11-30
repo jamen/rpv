164da2e864f848063ee4b499c1e7cf4ac350450a
Jamen Marz <jamenmarz@gmail.com>
2015-11-30 15:45:06 -0700

    Add lib/search.js test

--- test/search.js
+++ test/search.js
@@ -1,2 +1,32 @@
 import test from 'ava';
 import { search, tree, Rule, Property, Value } from '../lib';
+
+test('"search" function', ({same}) => {
+  // Create uncirculated tree
+  let prop = new Property('margin', [
+    new Value(10, 'px'),
+    new Value(2, 'em')
+  ]),
+
+  doc = [
+    new Rule('.foo', [
+      prop
+    ]),
+
+    new Rule('.bar', [
+      new Property('margin', [
+        new Value(5, 'px'),
+        new Value(1, 'em')
+      ])
+    ])
+  ];
+
+  // Set a tag to search
+  prop.set('foo-bar');
+
+  // Search tag
+  let [result] = search(doc, [ 'foo-bar' ]);
+
+  // See if it selected the right one.
+  same(result, prop);
+});
